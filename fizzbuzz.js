e6f624c7cee55e5c73b486e89f49dba76b4d84ee
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-14 14:46:11 -0500

    Created files

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+var avow = require('avow');
+
+function FizzBuzz (n) {
+    var output = '';
+    return output;
+
+    if 
+
+}
+
+
+avow('FizzBuzz 1 should be .', '.' === FizzBuzz(1), '.', FizzBuzz(1));
+avow('FizzBuzz 2 should be ..', '..' === FizzBuzz(2), '..', FizzBuzz(2));
+avow('FizzBuzz 3 should be ..Fizz', '..Fizz' === FizzBuzz(3), '..Fizz', FizzBuzz(3));
+avow('FizzBuzz 5 should be ..Fizz.Buzz', '..Fizz.Buzz' === FizzBuzz(5), '..Fizz.Buzz', FizzBuzz(5));
+avow('FizzBuzz 10 should be ..Fizz.BuzzFizz..FizzBuzz', '..Fizz.BuzzFizz..FizzBuzz' === FizzBuzz(10), '..Fizz.BuzzFizz..FizzBuzz', FizzBuzz(10));
+avow('FizzBuzz 15 should be ..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz', '..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz' === FizzBuzz(15), '..Fizz.BuzzFizz..FizzBuzz.Fizz..FizzBuzz', FizzBuzz(15));
---
+++ flow-control.js
@@ -0,0 +1,91 @@
+// var todayIs ='Tuesday';
+
+// if (todayIs === 'Tuesday') {
+//     console.log('Happy Valentines Day!');
+// }
+
+// else if (todayIs === 'Wednesday') {
+//     console.log('Happy Humpday!');
+// }
+
+// else {
+//     console.log('Happy ' + todayIs + '!');
+//     //console.log(`Happy ${todayIs}!`);
+// }
+
+
+// var todayIsTuesday = true;
+// //console.log(typeof todayIsTuesday); 
+
+
+// var counter = 0;
+// //counter++;
+// counter = counter + 2;
+// //counter += 2;
+// //console.log(counter);
+
+// if (counter >= 2) {
+//     console.log('Correct answer.');
+// }
+
+// var slicesOfPizza = 12;
+
+// if (slicesOfPizza % 3 === 0 || slicesOfPizza % 5 === 0) {
+//     console.log('There\'s enough for all of you.');
+// }
+
+// else if (slicesOfPizza % 3 === 0) {
+//     console.log('Enough for three.');
+// }
+
+// else {
+//     console.log('Not going to divide evenly.');
+// }
+
+// var temperature = 50;
+
+// switch (temperature) {
+//     case 50:
+//         console.log('Getting warmer.');
+//         break;
+//     case 60:
+//         console.log('Getting closer to Spring.');
+//         break;
+//     case 70:
+//         console.log('Spring is here.');
+//         break;
+//     default:
+//         console.log('Who knows. Welcome to Indiana.');
+// }
+
+// var favoriteColor = 'black';
+// var message = favoriteColor === 'red' ? 'My favorite color is red.' : 'My favorite color is not red.';
+
+// console.log(message);
+
+// var pets = ['dog', 'cat', 'hamster', 'fish', 'bird', 'snake', 'tiger'];
+
+// for (var i = 0; i < pets.length; i++) {
+//     console.log(pets[i]);
+// } 
+
+// for (var counter = 0; counter < 500; counter ++) {
+//     console.log('Hacked.' .repeat(counter));
+// } 
+
+// for (var i = 0; i < 20; i++) {
+//     document.getElementById('robots').innerHTML += `<img src="http://robohash.org/indy${i}" />`;
+// }
+
+// function addOne(number) {
+//     return number += 1;
+// }
+
+// var result = addOne(100); 
+
+// console.log(result);
+
+function FizzBuzz (n) {
+    var output = '';
+    return output;
+}
