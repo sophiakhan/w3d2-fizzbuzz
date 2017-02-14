bc7a838288da1f776da9e99992d2e55549433241
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-14 16:08:54 -0500

    updated output

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,6 +5,7 @@ function FizzBuzz (n) {
     var output = '';
 
     for (var i = 1; i <= n; i++) {
+    // tells the loop where to start, how many times to loop, and to go up in number 
         if (i % 3 === 0 && i % 5 === 0) {
             output += 'FizzBuzz';
         }
@@ -26,9 +27,9 @@ function FizzBuzz (n) {
     return output;
 
 }
-
 // calls the function and tells it how many times to run for n
-// FizzBuzz(15);
+FizzBuzz(15);
+
 
 
 avow('FizzBuzz 1 should be .', '.' === FizzBuzz(1), '.', FizzBuzz(1));
